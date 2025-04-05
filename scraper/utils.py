d61b46944c6194ede2ed55f7797713ebe7742df6
Jaren <tubaljaren09@gmail.com>
2025-04-05 19:58:55 +0800

    Add tempfile directory

--- scraper/utils.py
+++ scraper/utils.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
+import tempfile  # import tempfile to create a temporary directory
 import time
 
 def scrape_page_with_selenium(url):
@@ -10,6 +11,11 @@ def scrape_page_with_selenium(url):
     options.add_argument('--disable-gpu')
     options.add_argument("--window-size=1920,1080")
     options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36')
+    
+    # Create a temporary directory for Chrome's user data
+    temp_dir = tempfile.mkdtemp()
+    options.add_argument(f"--user-data-dir={temp_dir}")
+    
     driver = webdriver.Chrome(options=options)
     
     try:
@@ -24,7 +30,6 @@ def scrape_page_with_selenium(url):
         # Wait for new content to load after clicking the "Show More" button
         time.sleep(5)  # Adjust sleep time if necessary
 
-
         html = driver.page_source
 
     except Exception as e:
@@ -36,7 +41,7 @@ def scrape_page_with_selenium(url):
     # Use BeautifulSoup to parse the HTML
     soup = BeautifulSoup(html, 'html.parser')
     
-    target_div = soup.select('div.main-products-grid__results ul li ')  # Adjust selector based on the HTML structure
+    target_div = soup.select('div.main-products-grid__results ul li')  # Adjust selector based on the HTML structure
 
     if target_div:
         extracted_data = []
@@ -46,13 +51,8 @@ def scrape_page_with_selenium(url):
             
             data = {
                 'title': title.get_text(strip=True) if title else 'No title',
-                'price': price.get_text(strip=True) if title else 'No price'
+                'price': price.get_text(strip=True) if price else 'No price'
             }
             extracted_data.append(data)
 
         return {'data': extracted_data}  # Return the extracted data
-
-if __name__ == "__main__":
-    test_url = "https://dynaquestpc.com/collections/intel"
-    result = scrape_page_with_selenium(test_url)
-    print("Scraped Data with Selenium:", result)
